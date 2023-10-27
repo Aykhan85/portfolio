76c98f0f56bf062d49eb6ea08171fd1fea17f63c
OMEN <ayxannematov96@gmail.com>
2023-10-27 18:13:28 +0400

    todo app was changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <header>
             <div className="header__container">
                 <h5 className='text-light'>Hello I'm</h5>
-                <h1>Aykhan Nematov</h1>
+                <h1>Aykhan</h1>
                 <h5 className="text-light">Frontend Developer</h5>
                 <CTA className='cta' />
 
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,7 +3,7 @@ import errorPage from '../assets/404-not-found.png'
 import myTeamPage from '../assets/my-team-page.png'
 import booking from '../assets/booking.png'
 import imgUploader from '../assets/image-uploader.png'
-import toDo from '../assets/todo-app.png'
+import toDo from '../assets/todo.png'
 import carHub from '../assets/Screenshot_26.png'
 import promptopia from '../assets/promptopia.png'
 import nike from '../assets/nike.png'
@@ -12,7 +12,7 @@ const photos = [
     { name: 'Promptopia', img: promptopia, live: true, url: 'https://github.com/Aykhan85/promptopia', liveUrl: 'https://promptopia-three-sandy.vercel.app/' },
     { name: 'Nike', img: nike, live: true, url: 'https://github.com/Aykhan85/nike', liveUrl: 'https://nike-ten-rouge.vercel.app/' },
     { name: 'Image Uploader', img: imgUploader, live: true, url: "https://github.com/Aykhan85/image_uploader_api", liveUrl: 'https://image-uplader.onrender.com' },
-    { name: 'Todo App', img: toDo, live: true, url: 'https://github.com/Aykhan85/todo_backend', liveUrl: 'https://todo-app-boyu.onrender.com' },
+    { name: 'Todo App', img: toDo, live: true, url: 'https://github.com/Aykhan85/todo-next', liveUrl: 'https://todo-next-brown.vercel.app/' },
     { name: 'Interrior Consultant Page', img: interriorConsultant, live: true, url: 'https://github.com/Aykhan85/interrior-consultant', liveUrl: 'https://lustrous-cannoli-5d459a.netlify.app/' },
     { name: 'Error Page', img: errorPage, live: true, url: 'https://github.com/Aykhan85/404-Not-Found-devchallenge-', liveUrl: 'https://heartfelt-salamander-e9dfd5.netlify.app/' },
     { name: 'Team Page', img: myTeamPage, live: false, url: 'https://github.com/Aykhan85/team-page' },
