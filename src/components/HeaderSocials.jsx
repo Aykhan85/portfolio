a49576803f4b1490f51e9d1aea1c925404f3780b
OMEN <ayxannematov96@gmail.com>
2023-11-02 17:04:14 +0400

    link of linkedin was fixed

--- src/components/HeaderSocials.jsx
+++ src/components/HeaderSocials.jsx
@@ -4,7 +4,7 @@ import { FaGithub } from 'react-icons/fa'
 const HeaderSocials = () => {
     return (
         <div className='header__socials'>
-            <a href="https://linkedin.com" target='_blank' rel='noreferrer'><BsLinkedin /></a>
+            <a href="https://linkedin.com/in/aykhan-nematov" target='_blank' rel='noreferrer'><BsLinkedin /></a>
             <a href="https://github.com/Aykhan85?tab=repositories" target='_blank' rel='noreferrer'><FaGithub /></a>
             <a href="https://www.instagram.com/aykhan_nematov" target='_blank' rel='noreferrer'><BsInstagram /></a>
         </div>
